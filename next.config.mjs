1c8dd2b91de4321b188638504b19a3b8758ea93e
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2023-11-30 09:48:48 -0300

    feat: init content layer

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import { withContentlayer } from 'next-contentlayer'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    swcMinify: true
+  }
+}
+
+export default withContentlayer(nextConfig)
